f0c7ccd37e33af2b4b147c5f22df5ea50eb9aca2
ylia35 <113721223+ylia35@users.noreply.github.com>
2023-03-02 11:55:35 +0300

    Update mother_class.py

--- mother_class.py
+++ mother_class.py
@@ -1,11 +1,20 @@
+def counter(func):
+    def wrapper(*args, **kwargs):
+        wrapper.count += 1
+        print(f'---Функция {func.__name__} выполнилась {wrapper.count} раз---')
+        return func(*args, **kwargs)
+    wrapper.count = 0
+    return wrapper
+
 class post:
+    @counter
     def __init__(self, nikname, date, text):
         self.nikname = nikname
         self.date = date
         self.likes = 0
         self.text = text
         self.comments = []
-
+    @counter
     def add_likes(self, how_much):
         try:
             how_much = int(how_much)
@@ -14,15 +23,15 @@ class post:
             print("Количество лайков указывается числом, а не буквами!!!")
         else:
             self.likes += how_much
-
+    @counter
     def add_comment(self, comment_text):
         if 'сволоч' in comment_text.lower():
             pass
         else:
             self.comments.append(comment_text)
-
+    @counter
     def get_all_comments(self):
         return self.comments
-
+    @counter
     def __str__(self):
         return f'Nickname: {self.nikname}, date: {self.date}, text: {self.text} likes: {self.likes}, comments: {(self.comments[:3])}'
